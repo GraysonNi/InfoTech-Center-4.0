a678d5766c05660ee0a4febd6b3c7a80c2e263dd
Grayson Nichols <02grayson.nichols@gulllakecs.org>
2023-01-23 13:49:53 -0500

    Update welcome message

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,3 +1,11 @@
 # Programer: Grayson Nichols
 # Date: 1.20.2023
 # Program: Infotech Center Upgrades
+
+"""
+Our Welcome Screen will start our program letting
+drivers know that the Infotech Center 4.0 OS is loading
+"""
+
+print("\n\nWelcome - InfotechCenter 4.0")
+print("\nInfotech Center 4.0 OS is now loading")
