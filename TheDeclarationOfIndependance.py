ad73c13315d9b0e4b8f947b07218b3e8b5e3eb4f
Grayson Nichols <02grayson.nichols@gulllakecs.org>
2023-02-13 13:09:44 -0500

    Added weather and vehicleResponseSystem

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -2,3 +2,21 @@
 # Date: 2.8.2023
 # Program: Weather System Updates
 
+#Import libraries here
+import random
+
+#Create weather conditions in a list and choose it randomly
+def weather():
+    weatherForcast = ["Snow", "Blizzard", "Rain", "Fog", "Windy", "Icy", "Sunshine"]
+    weatherCondition = random.choice(weatherForcast)
+    return weatherCondition
+
+# Variable to call weather() once in our VRS
+weatherAlert = weather()
+
+print(weatherAlert)
+
+# VRS() to respond to the weather conditions
+def vehicleResponceSystem():
+    if weatherAlert == "Snow":
+        
