a2ba6932259895aacf76e79c2f894e74bade7b5c
Grayson Nichols <02grayson.nichols@gulllakecs.org>
2023-02-13 13:56:17 -0500

    Added more Elifs and an els to complete our list

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -14,9 +14,25 @@ def weather():
 # Variable to call weather() once in our VRS
 weatherAlert = weather()
 
-print(weatherAlert)
 
 # VRS() to respond to the weather conditions
 def vehicleResponceSystem():
     if weatherAlert == "Snow":
-        
+        print("\nNWS has changed your Alarm by 15 minutes because of the weather forcast of", weatherAlert)
+        print("VRS has been engaged only allowing your vehicle to go 45 MPH")
+    elif weatherAlert == "Blizzard":
+        print("\nNWS has changed your Alarm by 30 minutes because of the weather forcast of", weatherAlert)
+        print("VRS has been engaged only allowing your vehicle to go 35 MPH")
+    elif weatherAlert == "Rain":
+        print("\nNWS is calling for", weatherAlert, ", please drive extra careful")
+    elif weatherAlert == "Fog":
+        print("\nNWS is calling for", weatherAlert, ", please drive extra careful")
+    elif weatherAlert == "Windy":
+        print("\nNWS is calling for", weatherAlert, "conditions, please drive extra careful")
+    elif weatherAlert == "Icy":
+        print("\nNWS has changed your Alarm by 60 minutes because of the weather forcast of", weatherAlert)
+        print("VRS has been engaged only allowing your vehicle to go 25 MPH")
+    else:
+        print("\nNWS is calling for", weatherAlert,"drive safely and have a wonderful day!")
+
+vehicleResponceSystem()
