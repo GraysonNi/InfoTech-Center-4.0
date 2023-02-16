8d862cd9aa13bcb9845d7900ae909fb69229e126
Grayson Nichols <02grayson.nichols@gulllakecs.org>
2023-02-16 13:46:12 -0500

    Fixed errors with pressureSensor function

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -2,19 +2,25 @@
 # Date: 2.16.2023
 # Program: Infotech Center Upgrades
 
+import random
+
 def tirePressure():
   pressures = ["left front", "right front", "left back", "right back", "none"]
-  tireStatus = random.choice(pressures)
-  return(tirePressure)
+  tireState = random.choice(pressures)
+  return(tireState)
 
-def pressureSensor:
-  if tireStatus == "left front":
-    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
-  elif tire Status == "right front":
-    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
-  elif tire Status == "left back":
-    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
-  elif tire Status == "right back":
-    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
-  else:
-    print("All of your tires have safe pressures.")
+tireStatus = tirePressure()
+
+def pressureSensor():
+    if tireStatus == "left front":
+        print("\nYour", tireStatus, "tire has a low pressure, please fill it soon.")
+    elif tireStatus == "right front":
+        print("\nYour", tireStatus, "tire has a low pressure, please fill it soon.")
+    elif tireStatus == "left back":
+        print("\nYour", tireStatus, "tire has a low pressure, please fill it soon.")
+    elif tireStatus == "right back":
+        print("\nYour", tireStatus, "tire has a low pressure, please fill it soon.")
+    else:
+        print("\nAll of your tires are at safe pressures.")
+
+pressureSensor()
