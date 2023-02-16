3bebe1df6a8d0e242fdf8ec959cb15926f63e8e5
GraysonNi <123008610+GraysonNi@users.noreply.github.com>
2023-02-16 13:34:28 -0500

    Added tire Pressure and Pressure Sensor functions

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,3 +1,20 @@
 # Programer: Grayson Nichols
-# Date: 1.20.2023
+# Date: 2.16.2023
 # Program: Infotech Center Upgrades
+
+def tirePressure():
+  pressures = ["left front", "right front", "left back", "right back", "none"]
+  tireStatus = random.choice(pressures)
+  return(tirePressure)
+
+def pressureSensor:
+  if tireStatus == "left front":
+    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
+  elif tire Status == "right front":
+    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
+  elif tire Status == "left back":
+    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
+  elif tire Status == "right back":
+    print("Your", tireStatus, "tire has a low pressure, please fill it soon.")
+  else:
+    print("All of your tires have safe pressures.")
