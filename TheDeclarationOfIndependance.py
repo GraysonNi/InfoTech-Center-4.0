608fd539352745002627654b9930b67925fc63c1
GraysonNi <123008610+GraysonNi@users.noreply.github.com>
2023-02-17 14:12:14 -0500

    Added station and radio function

--- TheDeclarationOfIndependance.py
+++ TheDeclarationOfIndependance.py
@@ -1,33 +1,28 @@
 # Programer: Grayson Nichols
-# Date: 1.20.2023
+# Date: 2.17.2023
 # Program: Infotech Center Upgrades
-
-"""
-Our Welcome Screen will start our program letting
-drivers know that the Infotech Center 4.0 OS is loading
-"""
 
 # Import Libraries Here
 import time
-import sys
+import random
 
-timeToSleep = 2
-x = 0
-a = 0
+def station():
+    stationGenre = ["Hip-Hop", "Pop", "Country", "Latin", "Rock", "Electronic", "Heavy Metal"]
+    return stationGenre
+currentStation = station()
 
-
-
-print("\n\nWelcome - InfotechCenter 4.0\n")
-time.sleep(timeToSleep)
-#print("\nInfotech Center 4.0 OS is now loading")
-
-while x != 20:
-    x += 1
-    b = ("\033[1;33;40m Infotech center is now loading" + "." * a)
-    a = a + 1
-    sys.stdout.write('\r'+b)
-    time.sleep(0.5)
-    if a == 4:
-        a = 0
-    if x ==20:
-        print('\033[1;32;40;40m Retina Scan Complete - Access Granted!')
+def radio():
+    if currentStation == "Hip-Hop":
+        print("\n You are now listening to", currentStation, "Radio."
+    elif currntStation == "Pop":
+        print("\n You are now listening to", currentStation, "Radio."
+    elif currntStation == "Country":
+        print("\n You are now listening to", currentStation, "Radio."
+    elif currntStation == "Latin":
+        print("\n You are now listening to", currentStation, "Radio."
+    elif currntStation == "Rock":
+        print("\n You are now listening to", currentStation, "Radio."
+    elif currntStation == "Electronic":
+        print("\n You are now listening to", currentStation, "Radio."
+    else:
+        print("\n You are now listening to", currentStation, "Radio."
